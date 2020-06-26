63b64b75320226769e8280aedfabdeaa32cd3479
heritio <66847929+heritio@users.noreply.github.com>
2020-06-26 08:25:10 +0200

    Update script.js
    refactoring checkrequired from if else stament to make a function that takes in the values in an array for cleaner code

--- form validator/script.js
+++ form validator/script.js
@@ -17,39 +17,31 @@ function showSucces(input) {
   const formControl = input.parentElement;
   formControl.className = "form-control success";
 }
+
 //Emailvalidator with regex
 function isEmailValid(email) {
   const reg = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
   return reg.test(email);
 }
-//if statements
 
+//checkrequiredfields function
+function checkRequired(inputArr) {
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === "") {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSucces(input);
+    }
+  });
+}
+//getFieldname function
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+//eventlistener
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  if (userName.value === "") {
-    showError(userName, "Username is required");
-  } else {
-    showSucces(userName);
-  }
-
-  if (email.value === "") {
-    showError(email, "Email is required");
-  } else if (!isEmailValid(email.value)) {
-    showError(email, "Email is not valid");
-  } else {
-    showSucces(email);
-  }
-
-  if (passWord.value === "") {
-    showError(passWord, "Password is required");
-  } else {
-    showSucces(passWord);
-  }
-
-  if (passWord2.value === "") {
-    showError(passWord2, "Password2 is required");
-  } else {
-    showSucces(passWord2);
-  }
+checkRequired([username, email, password, password2]); 
 });
