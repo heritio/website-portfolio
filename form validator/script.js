be67bda129da57f2238b5c10e2b56d6668bcbf5d
heritio <66847929+heritio@users.noreply.github.com>
2020-06-26 08:51:39 +0200

    Update script.js
    adding a responsive message for email check

--- form validator/script.js
+++ form validator/script.js
@@ -20,7 +20,7 @@ function showSucces(input) {
 
 //Emailvalidator with regex
 function checkEmail(input) {
-  const reg = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+  const reg = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
   if (reg.test(input.value.trim())) {
     showSucces(input);
   } else {
@@ -67,4 +67,5 @@ form.addEventListener("submit", function (e) {
 checkRequired([username, email, password, password2]); 
    checkLength(username, 3, 15);
   checkLength(password, 6, 25);
+  checkEmail(email);
 });
